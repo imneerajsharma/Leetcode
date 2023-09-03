965ccce5c1d50943c7e9bbdc96f13ea7a8af110a
Neeraj Sharma <neerajkumarsharma279@gmail.com>
2023-09-03 11:19:33 -0400

    Array Rotation

---
+++ 75/Blind75/src/main/java/ArrayRotation.java
@@ -0,0 +1,19 @@
+public class ArrayRotation {
+    public static int findKRotation(int[] arr) {
+        int n = arr.length; //size of array.
+        int ans = Integer.MAX_VALUE, index = -1;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] < ans) {
+                ans = arr[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 6, 7, 0, 1, 2, 3};
+        int ans = findKRotation(arr);
+        System.out.println("The array is rotated " + ans + " times.");
+    }
+}
